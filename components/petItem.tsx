8f32fee38cbd2f60cd349d4140a4ee8c384f7a99
SAMUEL LUIS DOS SANTOS <samuel.lsantos4@senacsp.edu.br>
2026-04-29 20:41:52 -0300

    aula 29/04 - start

---
+++ components/petItem.tsx
@@ -0,0 +1,74 @@
+import { Animated, Image, StyleSheet, View } from 'react-native';
+import { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+
+export function PetItem({ pet }) {
+  const rotation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotate: `${rotation.value}deg` }],
+    };
+  });
+
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri: pet.images[0] }}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <Animated.View style={[animatedStyle, {
+        marginBottom: 4,
+      }]}>
+        <Animated.Text
+          style={styles.text}>
+          {pet.name}
+        </Animated.Text>
+      </Animated.View>
+
+      <Animated.Text
+        style={styles.subtitle}>
+        {pet.gender}
+      </Animated.Text>
+      <Animated.Text
+        style={styles.subtitle}>
+        {pet.age} anos
+      </Animated.Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#f8f9fa',
+    borderRadius: 12,
+    margin: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#666',
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+    marginBottom: 12,
+  }
+});
---
+++ data/pets.tsx
@@ -0,0 +1,286 @@
+ const pets = [
+    {
+      "_id": "6759e70256271bde9290f5c7",
+      "name": "Thor",
+      "breed": "Pastor alemao",
+      "gender": "male",
+      "age": 4,
+      "weight": 130,
+      "color": "Caramelo com capa preta",
+      "story": "Comprado em criador de raa pedrigree",
+      "images": [
+        "https://img.freepik.com/fotos-gratis/imagem-vertical-de-um-pastor-alemao-rodeado-por-vegetacao-com-um-fundo-desfocado_181624-9782.jpg?ga=GA1.1.2116784397.1723057156&semt=ais_hybrid",
+        "https://img.freepik.com/fotos-gratis/cao-pastor-alemao-idoso-com-a-lingua-de-fora-em-uma-area-nevada-com-um-fundo-desfocado_181624-19601.jpg?ga=GA1.1.2116784397.1723057156&semt=ais_hybrid"
+      ],
+      "user": {
+        "_id": "67543f23cfc59d4cb498c07f",
+        "name": "LucasRossatto",
+        "email": "rossatto@gmail.com",
+        "phone": "112340965",
+        "isAdmin": false,
+        "createdAt": "2024-12-07T12:27:15.361Z",
+        "updatedAt": "2024-12-07T12:27:15.361Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-11T19:24:50.492Z",
+      "updatedAt": "2024-12-11T19:41:28.237Z",
+      "__v": 0
+    },
+    {
+      "_id": "6759dd411e8a603fe3867a9a",
+      "name": "Basker",
+      "breed": "dobermann",
+      "gender": "male",
+      "age": 4,
+      "weight": 36,
+      "color": "Preto",
+      "story": "Basker e um dobermann fiel e guardiao",
+      "images": [
+        "https://midia.gruposinos.com.br/_midias/jpg/2024/07/29/cachorro_dobermann-21796196.jpg"
+      ],
+      "user": {
+        "_id": "67588bcc55ef514836139bd9",
+        "name": "felipe lima",
+        "email": "felipel@gmail.com",
+        "phone": "1234567891",
+        "isAdmin": false,
+        "createdAt": "2024-12-10T18:43:25.128Z",
+        "updatedAt": "2024-12-10T18:43:25.128Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-11T18:43:13.068Z",
+      "updatedAt": "2024-12-11T18:44:04.413Z",
+      "__v": 0
+    },
+    {
+      "_id": "6759da3156271bde9290f4f7",
+      "name": "Pegasus",
+      "breed": "Friesian",
+      "gender": "male",
+      "age": 10,
+      "weight": 200,
+      "color": "Preto",
+      "story": "Cavalo docil e carinhoso, muito bonito e forte.",
+      "images": [
+        "https://www.comprerural.com/wp-content/uploads/2024/02/image-20-3.jpg"
+      ],
+      "user": {
+        "_id": "6750a748c0f32c7550898df1",
+        "name": "joao",
+        "email": "joao123@gmail.com",
+        "phone": "40028922",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T19:02:33.228Z",
+        "updatedAt": "2024-12-04T19:02:33.228Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-11T18:30:09.028Z",
+      "updatedAt": "2024-12-11T18:33:37.302Z",
+      "__v": 0
+    },
+    {
+      "_id": "6759d30956271bde9290f4e2",
+      "name": "Pegasus",
+      "breed": "Friesian",
+      "gender": "male",
+      "age": 10,
+      "weight": 200,
+      "color": "Preto",
+      "story": "Cavalo docil e carinhoso, muito bonito e forte.",
+      "images": [
+        "https://www.comprerural.com/wp-content/uploads/2024/02/image-20-3.jpg"
+      ],
+      "user": {
+        "_id": "6750a748c0f32c7550898df1",
+        "name": "joao",
+        "email": "joao123@gmail.com",
+        "phone": "40028922",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T19:02:33.228Z",
+        "updatedAt": "2024-12-04T19:02:33.228Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-11T17:59:37.793Z",
+      "updatedAt": "2024-12-11T18:33:37.302Z",
+      "__v": 0
+    },
+    {
+      "_id": "6759cf031e8a603fe3867a1f",
+      "name": "Pegasus",
+      "breed": "Vira lata",
+      "gender": "male",
+      "age": 10,
+      "weight": 3,
+      "color": "Preto",
+      "story": "Lindo Pet. Encontrado na rua, abandonado. Mas, foi cuidado e está muito saúdavel e forte. Não tenho condições para cuidar, então gostaria de doar para uma família cuidadosa e que tenha tempo para dar atenção ao pet.",
+      "images": [
+        "https://www.whiskas.com.br/cdn-cgi/image/format=auto,q=90/sites/g/files/fnmzdf2156/files/2022-11/elever-un-Chaton-ses-premiers-mois.jpg",
+        "https://petanjo.com/blog/wp-content/uploads/2023/10/gato-sorri.png"
+      ],
+      "user": {
+        "_id": "6750a748c0f32c7550898df1",
+        "name": "joao",
+        "email": "joao123@gmail.com",
+        "phone": "40028922",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T19:02:33.228Z",
+        "updatedAt": "2024-12-04T19:02:33.228Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-11T17:42:27.994Z",
+      "updatedAt": "2024-12-11T18:33:37.302Z",
+      "__v": 0
+    },
+    {
+      "_id": "6759cd1b1e8a603fe38679df",
+      "name": "Felix",
+      "breed": "Rottweiler",
+      "gender": "male",
+      "age": 7,
+      "weight": 20,
+      "color": "Preto",
+      "story": "Rottweiler é uma raça de cães molossos desenvolvida na Alemanha.",
+      "images": [
+        "https://petcare.com.br/wp-content/uploads/2022/12/100.jpg"
+      ],
+      "user": {
+        "_id": "6750a47ecfc59d4cb498be27",
+        "name": "Danilo",
+        "email": "danilo@gmail.com",
+        "phone": "11987654321",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T18:50:38.411Z",
+        "updatedAt": "2024-12-04T18:50:38.411Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-11T17:34:19.311Z",
+      "updatedAt": "2024-12-11T17:36:04.625Z",
+      "__v": 0
+    },
+    {
+      "_id": "6759c9ae56271bde9290f413",
+      "name": "Meu pet - NewInfos",
+      "breed": "Vira lata",
+      "gender": "male",
+      "age": 12,
+      "weight": 3,
+      "color": "Cinza & Branco",
+      "story": "Lindo Pet. Encontrado na rua, abandonado. Mas, foi cuidado e está muito saúdavel e forte. Não tenho condições para cuidar, então gostaria de doar para uma família cuidadosa e que tenha tempo para dar atenção ao pet.",
+      "images": [
+        "https://www.whiskas.com.br/cdn-cgi/image/format=auto,q=90/sites/g/files/fnmzdf2156/files/2022-11/elever-un-Chaton-ses-premiers-mois.jpg",
+        "https://petanjo.com/blog/wp-content/uploads/2023/10/gato-sorri.png"
+      ],
+      "user": {
+        "_id": "6750a663c0f32c7550898dc1",
+        "name": "Kawan",
+        "email": "kawanwagnner7@gmail.com",
+        "phone": "11 939393939",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T18:58:43.924Z",
+        "updatedAt": "2024-12-04T18:58:43.924Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-11T17:19:42.442Z",
+      "updatedAt": "2024-12-11T17:36:04.625Z",
+      "__v": 0
+    },
+    {
+      "_id": "6758a22860ed7af1fa155da9",
+      "name": "Meu Pet 2",
+      "breed": "Qualquer",
+      "gender": "male",
+      "age": 12,
+      "weight": 3,
+      "color": "VErde",
+      "story": "Lindo pet. Encontrado na rua, abandonado. MAs muito saudável.",
+      "images": [
+        "https://tpdedu.s3.ap-southeast-2.amazonaws.com/uploads/2024/03/27192027/bird-scaled.jpg",
+        "https://t4.ftcdn.net/jpg/05/65/36/03/360_F_565360370_LrWWCTxczrmwqpsPYPljiFyE4gFqpecr.jpg"
+      ],
+      "user": {
+        "_id": "6750a38ecfc59d4cb498bdfc",
+        "name": "Samuel Santos",
+        "email": "samuel@email.com",
+        "phone": "1191234-89786",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T18:46:38.819Z",
+        "updatedAt": "2024-12-04T18:46:38.819Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-10T20:18:48.307Z",
+      "updatedAt": "2024-12-11T17:36:04.625Z",
+      "__v": 0
+    },
+    {
+      "_id": "6758a21560ed7af1fa155da7",
+      "name": "Meu Pet 2",
+      "breed": "Qualquer",
+      "gender": "male",
+      "age": 12,
+      "weight": 3,
+      "color": "VErde",
+      "story": "Lindo pet. Encontrado na rua, abandonado. MAs muito saudável.",
+      "images": [
+        "https://tpdedu.s3.ap-southeast-2.amazonaws.com/uploads/2024/03/27192027/bird-scaled.jpg",
+        "https://t4.ftcdn.net/jpg/05/65/36/03/360_F_565360370_LrWWCTxczrmwqpsPYPljiFyE4gFqpecr.jpg"
+      ],
+      "user": {
+        "_id": "6750a38ecfc59d4cb498bdfc",
+        "name": "Samuel Santos",
+        "email": "samuel@email.com",
+        "phone": "1191234-89786",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T18:46:38.819Z",
+        "updatedAt": "2024-12-04T18:46:38.819Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-10T20:18:29.075Z",
+      "updatedAt": "2024-12-11T17:36:04.625Z",
+      "__v": 0
+    },
+    {
+      "_id": "67588c8955ef514836139be5",
+      "name": "Vitao",
+      "age": 5,
+      "weight": 10,
+      "color": "Preto e Branco",
+      "images": [
+        "https://www.thesprucepets.com/thmb/2K97c7-05caG1cASgKYAvlD1hV4=/4000x2667/filters:fill(auto,1)/husky-dog-lying-in-the-915124768-34493760df9648f99961b50a16c39529.jpg"
+      ],
+      "user": {
+        "_id": "6750a3fdc0f32c7550898d2a",
+        "name": "victor",
+        "email": "victorsantos@gmail.com",
+        "phone": "12345678",
+        "isAdmin": false,
+        "createdAt": "2024-12-04T18:48:30.032Z",
+        "updatedAt": "2024-12-04T18:48:30.032Z",
+        "__v": 0
+      },
+      "isVerified": true,
+      "available": true,
+      "createdAt": "2024-12-10T18:46:33.340Z",
+      "updatedAt": "2024-12-10T18:48:04.995Z",
+      "__v": 0
+    }
+  ]
+
+  export default pets;
